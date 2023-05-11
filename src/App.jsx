dc64897ef4b276d89a236e0ac34e6f50b0e5fe65
arkawWebDev <arkar84848484@gmail.com>
2023-05-11 23:32:18 +0630

    use reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const firstReducer = (state, action) => {
+  switch (action.type) {
+    case "minus":
+      return { ...state, count: state.count - 1 };
+    case "plus":
+      return { ...state, count: state.count + 1 };
+    case "updateKey":
+      return { ...state, key: action.payload };
+    default:
+      throw new Error();
+  }
+};
+
+const ACTION = {
+  PLUS: "plus",
+  MINUS: "minus",
+  UPDATE_KEY: "updateKey",
+};
 
 function App() {
-  const [key, setKey] = useState("");
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(firstReducer, { count: 0, key: "" });
 
   return (
     <div className="App">
       <input
         type="text"
-        onChange={(e) => {
-          setKey(e.target.value);
-        }}
+        onChange={(e) =>
+          dispatch({ type: ACTION.UPDATE_KEY, payload: e.target.value })
+        }
       />
-      <h1>your key is - {key}</h1>
-      <button
-        onClick={() => {
-          setCount((prev) => prev - 1);
-        }}
-      >
-        -
-      </button>
-      <span>{count}</span>
-      <button
-        onClick={() => {
-          setCount((prev) => prev + 1);
-        }}
-      >
-        +
-      </button>
+      <h1>your key is - {state.key}</h1>
+      <button onClick={() => dispatch({ type: ACTION.MINUS })}>-</button>
+      <span>{state.count}</span>
+      <button onClick={() => dispatch({ type: ACTION.PLUS })}>+</button>
     </div>
   );
 }
